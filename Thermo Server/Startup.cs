9ee292e720b82ee2c33579617eaf5736be697f85
Alex Legio <legiosys@gmail.com>
2020-05-23 13:54:20 +0300

    Swagger

--- Thermo Server/Startup.cs
+++ Thermo Server/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
+using System.IO;
 
 namespace Thermo_Server
 {
@@ -30,7 +32,12 @@ namespace Thermo_Server
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Thermo Api", Version = "v1" });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
