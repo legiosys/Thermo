1085dee42bc89899a26dbc40245b3ad6077dfe93
Alex Legio <legiosys@gmail.com>
2020-05-23 13:55:12 +0300

    Raspi Controller + DTO RaspiTemp

---
+++ Thermo Server Domain/DTO/RaspiTemp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thermo_Server_Domain.DTO
+{
+    public class RaspiTemp
+    {
+        public string SensorId { get; set; }
+        public float Temp { get; set; }
+    }
+}
---
+++ Thermo Server/Controllers/RaspiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Thermo_Server_Domain.DTO;
+
+namespace Thermo_Server.Controllers
+{
+    [Produces("application/json")]
+    [Route("[controller]")]
+    [ApiController]
+    public class RaspiController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        public RaspiController(ILogger<RaspiController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>Adds temperature to db</summary>
+        /// <remarks>Sample request: 
+        /// 
+        ///     POST {
+        ///         "SensorId": "human",
+        ///         "Temp" : 36.6
+        ///     }
+        ///     
+        /// </remarks>
+        [HttpPost("temp")]
+        public async Task<ActionResult<RaspiTemp>> PostTemp(RaspiTemp temp)
+        {
+            return temp;
+        }
+    }
+}
