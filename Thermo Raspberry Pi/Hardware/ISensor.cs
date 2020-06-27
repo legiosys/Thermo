46ad8593035a4fe0201a7f0324eef420dc16624f
Alex Legio <legiosys@gmail.com>
2020-06-27 16:51:47 +0300

    Added hardware interface and mock sensors

---
+++ Thermo Raspberry Pi/Hardware/ISensor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Thermo_Raspberry_Pi.Hardware
+{
+    public interface ISensor
+    {
+        public IEnumerable<Temperature> GetTemperatures();
+    }
+}
