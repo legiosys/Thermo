0541904daa3aa05756aed2b63d7234db22256f1b
Alex Legio <legiosys@gmail.com>
2020-06-27 16:52:34 +0300

    Add Http connector interface and sender to server

---
+++ Thermo Raspberry Pi/Web/Sender.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Flurl.Http;
+
+namespace Thermo_Raspberry_Pi.Web
+{
+    public class Sender : IConnector
+    {
+        public string BaseUrl = "http://localhost:8000/Raspi/";
+        public async Task AddTemperature(string userKey, IEnumerable<Temperature> temperatures)
+        {
+            var url = string.Concat(BaseUrl, "Temp");
+            foreach(var temp in temperatures)
+            {
+                await url.PostJsonAsync(new { UserKey = userKey, temp.HardwareId, temp.Value });
+            }
+        }
+
+        public async Task<int> CheckUser(string user)
+        {
+            var url = string.Concat(BaseUrl, "User/", user);
+            return await url.GetJsonAsync<int>();
+        }
+
+        public async Task AddUser(string userKey, IEnumerable<string> sensors)
+        {
+            var url = string.Concat(BaseUrl, "User");
+            await url.PostJsonAsync(new { UserKey = userKey, Sensors = sensors });
+        }
+    }
+}
